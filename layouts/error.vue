dadd0cc674638e38e55f0f237bb8989694dc6740
anon-kae <anon.ksl42@gmail.com>
2022-07-19 12:20:59 +0700

    feat: modified layout of error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <v-row justify="center" align="center">
+    <div v-if="error.statusCode === 404" align="center">
+      <v-img
+        :src="require('@/assets/images/PageNotFound.png')"
+        max-width="350px" />
+      <h2>
+        {{ pageNotFound }}
+      </h2>
+      <h4>
+        <nuxt-link to="/">
+          Home page
+        </nuxt-link>
+      </h4>
+    </div>
+    <div v-else align="center">
+      <v-img :src="require('@/assets/images/Error.png')" max-width="350px" />
+      <h2>
+        {{ otherError }}
+      </h2>
+      <p>Our experts are working to fix the issue.</p>
+      <h4>
+        <nuxt-link to="/">
+          Home page
+        </nuxt-link>
+      </h4>
+    </div>
+  </v-row>
 </template>
 
 <script>
@@ -20,13 +38,13 @@ export default {
       default: null,
     },
   },
-  data() {
+  data () {
     return {
       pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+      otherError: 'Oops! Something went wrong here',
     }
   },
-  head() {
+  head () {
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
